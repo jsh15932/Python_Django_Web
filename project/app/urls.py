4be99649d4e4fb0f7dc0371069f80268b2b7a966
장승훈 <jsh1400512@naver.com>
2020-05-31 19:53:00 +0900

    Commit

--- project/app/urls.py
+++ project/app/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('new/', views.new, name='new'),
     path('detail/<int:blog_id>/', views.detail, name='detail'),
+    path('create/', views.create, name='create'),
+    path('postcreate/', views.postcreate, name='postcreate'),
 ]
