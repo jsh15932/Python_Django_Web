1d11d1dd1ea3884091fae5a06910f187146bc7e7
장승훈 <jsh1400512@naver.com>
2020-06-04 05:51:24 +0900

    Commit

--- project/project/urls.py
+++ project/project/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 import app.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', app.views.home, name='home'),
     path('app/', include('app.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
