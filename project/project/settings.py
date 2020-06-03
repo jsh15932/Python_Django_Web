8ccffdeeca9f6cf6fd1b0c2ae52ec932307d1866
장승훈 <jsh1400512@naver.com>
2020-06-04 06:31:27 +0900

    Commit

--- project/project/settings.py
+++ project/project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app.apps.AppConfig',
+    'member.apps.MemberConfig',
 ]
 
 MIDDLEWARE = [
--- project/project/urls.py
+++ project/project/urls.py
@@ -3,9 +3,11 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 import app.views
+import member.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', app.views.home, name='home'),
     path('app/', include('app.urls')),
+    path('member/', include('member.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
